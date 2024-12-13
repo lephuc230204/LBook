83ea8174b3ddafab7bcd0473c524627df5eecf9c
NguyenLam1708 <lam170804@gmail.com>
2024-12-13 16:17:06 +0700

    create entity cart, cartItem

---
+++ src/main/java/com/example/lbook/entity/Cart.java
@@ -0,0 +1,33 @@
+package com.example.lbook.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table( name = "cart")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDate createdDate;
+
+    @OneToMany(mappedBy = "cart")
+    private List<CartItem> items = new ArrayList<>();
+}
---
+++ src/main/java/com/example/lbook/entity/CartItem.java
@@ -0,0 +1,36 @@
+package com.example.lbook.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    private int quantity;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+}
